eeb45ec81a99d0f60ffeda7482577b5c1800793c
ImmortalCount <devnaut@gmail.com>
2022-09-28 16:00:15 -0700

    profile updated

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,22 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:theory_mountain/services/auth.dart';
+import 'package:provider/provider.dart';
+import 'package:theory_mountain/services/services.dart';
+import 'package:theory_mountain/shared/shared.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
   @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
-      ),
-      body: ElevatedButton(
-          child: const Text('signout'),
-          onPressed: () async {
-            await AuthService().signOut();
-            Navigator.of(context)
-                .pushNamedAndRemoveUntil('/', (route) => false);
-          }),
-    );
+    var report = Provider.of<Report>(context);
+    var user = AuthService().user;
+
+    if (user != null) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrange,
+          title: Text(user.displayName ?? 'Guest'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                margin: const EdgeInsets.only(top: 50),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: NetworkImage(user.photoURL ??
+                        'https://www.gravatar.com/avatar/placeholder'),
+                  ),
+                ),
+              ),
+              Text(user.email ?? '',
+                  style: Theme.of(context).textTheme.headline6),
+              const Spacer(),
+              Text('${report.total}',
+                  style: Theme.of(context).textTheme.headline2),
+              Text('Quizzes Completed',
+                  style: Theme.of(context).textTheme.subtitle2),
+              const Spacer(),
+              ElevatedButton(
+                child: const Text('logout'),
+                onPressed: () async {
+                  await AuthService().signOut();
+                  if (mounted) {
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil('/', (route) => false);
+                  }
+                },
+              ),
+              const Spacer(),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return const Loader();
+    }
   }
 }
